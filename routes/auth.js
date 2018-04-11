a19630d58a2a09433f5f36b8015dc813815c0fb4
elysianalz <mtk2011@live.com.au>
2018-04-11 15:43:31 +1000

    add registration post route registration complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,23 @@
 var express = require("express");
 var router = express.Router({mergeParams: true});
+var passport = require("passport");
 var User = require("../models/user");
 
 //registration routes
 router.get("/register", function(req, res){
 	res.render("register");
+});
+
+router.post("/register", function(req, res){
+	User.register(new User({username: req.body.username, email: req.body.email}), req.body.password, function(err, newUser){
+		if(err || !newUser){
+			console.log(err.message);
+			return res.redirect("back");
+		} 
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/blogs");
+		});
+	});
 });
 
 module.exports = router;
