32cb99d4f89b409aa6e6baa0f8b526680fe0b55d
elysianalz <mtk2011@live.com.au>
2018-04-11 19:48:44 +1000

    add edit route for blog add delete route for comment

--- routes/index.js
+++ routes/index.js
@@ -63,6 +63,30 @@ router.delete("/blogs/:id/delete", function(req, res){
 	})
 });
 
+//edit blog routes
+router.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err || !foundBlog){
+			req.flash("error", err.message);
+			res.redirect("back");
+		} else {
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+});
+
+router.put("/blogs/:id/edit", function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, newBlog){
+		if(err || !newBlog){
+			req.flash("err", err.message);
+			res.redirect("back");
+		} else {
+			req.flash("success", "Successfully updated blog");
+			res.redirect("/blogs/"+req.params.id);
+		}
+	});
+});
+
 //comment on blog
 router.post("/blogs/:id/comment/new", function(req, res){
 	Blog.findById(req.params.id, function(err, foundBlog){
@@ -83,6 +107,18 @@ router.post("/blogs/:id/comment/new", function(req, res){
 					res.redirect("/blogs/"+req.params.id);
 				}
 			});
+		}
+	});
+});
+
+//delete comment
+router.delete("/blogs/:id/:comment_id/delete", function(req, res){
+	Comment.findByIdAndRemove(req.params.comment_id, function(err, info){
+		if(err){
+			req.flash("error", err.message);
+		} else {
+			req.flash("success", "Successfully deleted comment");
+			res.redirect("/blogs/"+ req.params.id);
 		}
 	});
 });
