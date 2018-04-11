bfcbccf2c99f58eb3e5bba29e916d6e4c4a68352
elysianalz <mtk2011@live.com.au>
2018-04-11 13:58:27 +1000

    add User model and implement passport

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var User = require("./models/user");
+
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.set(methodOverride("_method"));
+app.use(express.static(__dirname+ "/public"));
+
+mongoose.connect("mongodb://localhost:27017/blog_test");
+
+app.use(require("express-session")({
+	secret: "this is a sample blog",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser());
+
+app.listen(5000, function(req, res){
+	console.log("test blog server has started");
+});
+
