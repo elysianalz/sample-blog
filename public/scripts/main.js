cc6953d3cf11251d848d88c3d9f90bc5eb8edb61
elysianalz <mtk2011@live.com.au>
2018-04-11 22:56:37 +1000

    style draft for show page of blog

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,10 +2,14 @@ $("#show-edit").click(function(){
 	$("#edit-comment-form").show();
 	$("#cancle-button").show();
 	$("#show-edit").hide();
+	$("#comment-text").hide();
+	$("#delete-button").hide();
 });
 
 $("#cancle-button").click(function(){
 	$("#edit-comment-form").hide();
 	$("#cancle-button").hide();
 	$("#show-edit").show();
+	$("#comment-text").show();
+	$("#delete-button").show();
 });
