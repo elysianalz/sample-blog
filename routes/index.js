0b758414af4a211a9cd03c6e2c78b5841493c4a4
elysianalz <mtk2011@live.com.au>
2018-04-11 17:05:42 +1000

    add create blog route, fix create blog form route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router({mergeParams: true});
 var Blog = require("../models/blog");
 
+//home routes
 router.get("/", function(req, res){
 	res.redirect("/blogs");
 });
@@ -10,8 +11,22 @@ router.get("/blogs", function(req, res){
 	res.render("home");
 });
 
+//create new blog routes
 router.get("/blogs/new", function(req, res){
 	res.render("new");
+});
+
+router.post("/blogs", function(req, res){
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err || !newBlog){
+			req.flash("error", err.message);
+			return res.redirect("back");
+		}
+		newBlog.author = req.user.username;
+		console.log(newBlog);
+		req.flash("success", "Blog successfully created");
+		res.redirect("/blogs");
+	});
 });
 
 module.exports = router;
